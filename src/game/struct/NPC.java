1b64ec7b7024950a0dee9f359cc917a1f3452845
Jack-Pumpkinhead <CowardlyLion1620@gmail.com>
2018-10-06 11:27:45 +0800

    add NPC

---
+++ src/game/struct/NPC.java
@@ -0,0 +1,13 @@
+package game.struct;
+
+import game.base.Player;
+import game.base.Scene;
+
+/**
+ * Created by CowardlyLion on 2018/10/6 11:25
+ */
+public class NPC extends Player {
+    public NPC(String name, String description, Scene currentScene) {
+        super(name, description, currentScene);
+    }
+}
